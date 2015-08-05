0ced1cc0ddd800f3bba72d8d959089d6886c2fcc
Michael Zadra <michaelzadra1@gmail.com>
2015-08-05 14:48:00 -0400

    Adds basic front end UI

--- public/js/controllers/search.js
+++ public/js/controllers/search.js
@@ -2,4 +2,12 @@ myApp.controller('SearchController', function($scope, $location) {
 	// Set animation class
 	$scope.pageClass = 'search';
 
+	//Default radio button is single
+	$scope.carMode = 'single';
+
+	// Returns single car view, or compare car view, based on radio button selection
+	$scope.carModeView = function () {
+		return 'partials/' + $scope.carMode + '.html';
+	};
+
 }); //SearchController 
